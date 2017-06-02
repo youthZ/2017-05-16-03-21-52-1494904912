3460f8b847e9bd0418cc89d028fd5d9e88373720
youths <552099787@qq.com>
2017-06-02 10:14:09 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 module.exports = function main(email, suffixes) {
-	var suffixe = email.split(".")[0].split("@")[1];
+	var suffixe = email.split("@")[1];
 	for (var index = 0; index < suffixes.length ; index++)
 	{
 		if(suffixe == suffixes[index])
